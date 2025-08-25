06b75e3cc22990ee3a4a1767d9c216d58af91d33
lwvu239 <lwvu239@gmail.com>
2025-08-25 22:39:35 +0700

    update code 25 lan 2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,15 +2,7 @@ const connection = require('../config/database');
 
 
 const getHomePage = (req, res) => {
-    let users = [];
-    connection.query(
-      'select * from Users u ;',
-      function (err, results, fields) {
-        users = results;
-        console.log(results);
-        res.send(JSON.stringify(users));
-      }
-    );
+    return res.render('home.ejs');
 } 
 
 const getTest = (req, res) => {
